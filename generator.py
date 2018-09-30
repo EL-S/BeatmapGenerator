1c0315ae664624b44ed3f61183acb62383a5e909
EL-S <31227083+EL-S@users.noreply.github.com>
2018-09-30 23:56:16 +1000

    Maximum Distance Between Notes
    A distance between notes has between established to remove random erractic jumps

--- generator.py
+++ generator.py
@@ -5,17 +5,28 @@ height = 1080
 notes = []
 
 class Note:
+    prev_x_value = randint(0,width)
+    spacing = round(width/5)
     def __init__(self):
-        self.x = randint(0,width)
+        lower = Note.prev_x_value-Note.spacing
+        upper = Note.prev_x_value+Note.spacing
+        if lower < 0:
+            lower = 0
+        if upper > width:
+            upper = width
+        self.x = randint(lower,upper)
         self.y = 0
+        Note.prev_x_value = self.x
     def pos(self):
         return (self.x,self.y)
 
 for i in range(100):
     notes.append(Note())
 
+x = 0
 for i in notes:
-    print(i.pos())
+    print(i.pos(),abs(i.pos()[0]-x))
+    x = i.pos()[0]
 
 print(len(notes))
 
